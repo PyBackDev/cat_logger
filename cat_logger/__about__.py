b3a20484e5135dccf11c3783e170b4383b8a4cc8
PyBackDev <evbalbukova@gmail.com>
2025-02-14 19:46:07 +0300

    Refactoring code

--- cat_logger/__about__.py
+++ cat_logger/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-present bev <bev@hpclab.local>
+# SPDX-FileCopyrightText: 2025-present PyBackDev <evbalbukova@gmail.com>
 #
 # SPDX-License-Identifier: MIT
 __version__ = "0.0.1"
